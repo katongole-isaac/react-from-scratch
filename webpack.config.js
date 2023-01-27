debbbc301ebcc7dd521e57d21e2845acbcf563a7
katongole isaac <katongolelsaac78@gmail.com>
2023-01-27 15:41:35 +0300

    resolve-url-loader added

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,9 @@ module.exports = {
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        // the resolve-url-loader should be used between css-loader and sass-loader.
+        // it is useful to resolve imported assets not relative to the main entry of scss files.
+        use: [MiniCssExtractPlugin.loader, "css-loader", "resolve-url-loader","sass-loader"],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
