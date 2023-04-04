b5e7dd86adbc72a54a62d0136e16837549f68289
katongole isaac <katongolelsaac78@gmail.com>
2023-04-04 20:13:21 +0300

    relative bundle file path error fixed

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,12 @@
 const { merge } = require("webpack-merge");
 const webpackConfig = require("./webpack.config");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+// Used when hosting the app on github pages.
+// *** Used to resolve the root path when hosted on github pages
+// It is used in webpack bundle to resolve file path relative to the dir suchas './' instead of '/'
+webpackConfig.output.publicPath = "./";
+
 module.exports = merge(webpackConfig, {
   mode: "production",
   devtool: "source-map",
